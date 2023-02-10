3da6900d4776e435dd4cc6ee0743fba85dc59053
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2023-02-10 08:44:48 +0100

    Add project files.

---
+++ ConsoleApp22/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift_3._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int intNr1 = Convert.ToInt32(Console.ReadLine());
+
+            if (intNr1 < 16)
+                Console.WriteLine("Du är för ung");
+            else if (intNr1 > 19)
+                Console.WriteLine("Du är för gammal");
+            else
+                Console.WriteLine("Du får delta i tävlingen");
+        }
+    }
+}
